f38f467f8641660b2fa891b671ee975b12d841cc
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-05-10 10:09:47 -0400

    initialized scala project with roman converter object

---
+++ src/test/scala/converterSpec.scala
@@ -0,0 +1,11 @@
+import converter.Roman
+import org.scalatest._
+
+class converterSpec extends FlatSpec with Matchers {
+
+  // val babysitterCalculator = Calculator
+  //
+  // "A Babysitter" should "be paid $12 for the hour from 6 to 7" in {
+  //   babysitterCalculator(18, 19, 22) shouldBe 12
+  // }
+}
